1617b3c9a08c034d3067fed85431d76ea26960fb
Blair Neal <laserpilot@gmail.com>
2011-10-06 10:40:40 -0400

    Initial Upload

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,131 +1,165 @@
 #include "testApp.h"
 
+const int width = 800;
+const int height = 600;
+
+GLfloat lightOnePosition[] = {40.0, 40, 100.0, 0.0};
+GLfloat lightOneColor[] = {0.99, 0.99, 0.99, 1.0};
+
+GLfloat lightTwoPosition[] = {-40.0, 40, 100.0, 0.0};
+GLfloat lightTwoColor[] = {0.99, 0.99, 0.99, 1.0};
+
 //--------------------------------------------------------------
 void testApp::setup(){
-        
-        ofSetVerticalSync(true);
-        //ofEnableAlphaBlending();
-        ofSetLogLevel(OF_LOG_VERBOSE);
-        
-        camWidth = 640;
-        camHeight = 480;
-        vidGrabber.setVerbose(true);
-        vidGrabber.initGrabber(camWidth,camHeight);
-        
-        colorImg.allocate(camWidth,camHeight);
-        grayImg.allocate(camWidth,camHeight);
-        grayBg.allocate(camWidth,camHeight);
-        grayDiff.allocate(camWidth,camHeight);
-        threshold = 120;
-        
-        ofBackground(0, 0, 0);
+	counter = 0;
+	//ofSetCircleResolution(4);
+	
+    ofSetVerticalSync(true);
+    //ofEnableAlphaBlending();
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    glShadeModel (GL_SMOOTH);
     
-    ofSetFrameRate(60);
+    camWidth = 640;
+    camHeight = 480;
+    vidGrabber.setVerbose(true);
+    vidGrabber.initGrabber(camWidth,camHeight);
     
-    mainOutputSyphonServer.setName("Screen Output");
+    colorImg.allocate(camWidth,camHeight);
+    grayImg.allocate(camWidth,camHeight);
+    grayBg.allocate(camWidth,camHeight);
+    grayDiff.allocate(camWidth,camHeight);
+    threshold = 120;
     
-        
-
-	bLearnBakground = true;
-
+    /* initialize lighting */
+    glLightfv (GL_LIGHT0, GL_POSITION, lightOnePosition);
+    glLightfv (GL_LIGHT0, GL_DIFFUSE, lightOneColor);
+    glEnable (GL_LIGHT0);
+    glLightfv (GL_LIGHT1, GL_POSITION, lightTwoPosition);
+    glLightfv (GL_LIGHT1, GL_DIFFUSE, lightTwoColor);
+    glEnable (GL_LIGHT1);
+    glEnable (GL_LIGHTING);
+    glColorMaterial (GL_FRONT_AND_BACK, GL_DIFFUSE);
+    glEnable (GL_COLOR_MATERIAL);
+    
+	
+    bSmooth = false;
+	ofSetWindowTitle("graphics example");
+    
+	mainOutputSyphonServer.setName("Screen Output");
+    
+	ofSetFrameRate(60); // if vertical sync is off, we can go a bit fast... this caps the framerate at 60fps.
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-	ofBackground(100,100,100);
-
+	counter = counter + 0.033f;
+    
+    ofBackground(100,100,100);
+    
     bool bNewFrame = false;
 
-	#ifdef _USE_LIVE_VIDEO
-       vidGrabber.grabFrame();
-	   bNewFrame = vidGrabber.isFrameNew();
-    #else
-        vidPlayer.idleMovie();
-        bNewFrame = vidPlayer.isFrameNew();
-	#endif
+    vidGrabber.grabFrame();
+    bNewFrame = vidGrabber.isFrameNew();
 
+    
 	if (bNewFrame){
-
-		#ifdef _USE_LIVE_VIDEO
-            colorImg.setFromPixels(vidGrabber.getPixels(), camWidth,camHeight);
-	    #else
-            colorImg.setFromPixels(vidPlayer.getPixels(), camWidth,camHeight);
-        #endif
+        
+        colorImg.setFromPixels(vidGrabber.getPixels(), camWidth,camHeight);
 
         grayImg = colorImg;
 		if (bLearnBakground == true){
 			grayBg = grayImg;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
 			bLearnBakground = false;
 		}
-
+        
 		// take the abs value of the difference between background and incoming and then threshold:
 		grayDiff.absDiff(grayBg, grayImg);
 		grayDiff.threshold(threshold);
-
+        
 		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
 		// also, find holes is set to true so we will get interior contours as well....
 		contourFinder.findContours(grayDiff, 20, (camWidth*camHeight)/3, 10, true);	// find holes
 	}
-    
 
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+	
+    // Clear with alpha, so we can capture via syphon and composite elsewhere should we want.
+    ofEnableSmoothing();
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    ofNoFill();
+    ofSetLineWidth(1);
     
-	// draw the incoming, the grayscale, the bg and the thresholded difference
-	ofSetHexColor(0xffffff);
-    //colorImg.draw(0,0,ofGetWidth(),0.75*ofGetWidth());
+
+    //Draw Video
+    colorImg.draw(0,0,ofGetWidth(),ofGetHeight());
     
+    //Get distance between all different centroids
+    for(int i=0; i<(int)contourFinder.blobs.size(); i++ ) {
+        for (int j=0; j<(int)contourFinder.blobs.size(); j++ ) {
+        connectDist[i][j] = ofDist(ofMap(contourFinder.blobs[i].centroid.x,0, camWidth,0,ofGetWidth()),
+                                ofMap(contourFinder.blobs[i].centroid.y,0,camHeight,0,.75*ofGetWidth()),
+                                ofMap(contourFinder.blobs[j].centroid.x,0, camWidth,0,ofGetWidth()),
+                                ofMap(contourFinder.blobs[j].centroid.y,0,camHeight,0,.75*ofGetWidth()));
+        }
+    }
+        
+   //Draw Circles and Lines
     for( int i=0; i<(int)contourFinder.blobs.size(); i++ ) {
-        ofNoFill();
-        for( int j=0; j<contourFinder.blobs[i].nPts; j++ ) {
+        ofSetColor(i*20,i*25,i*20);
             mapCentX = ofMap(contourFinder.blobs[i].centroid.x,0,camWidth,0,ofGetWidth());
             mapCentY = ofMap(contourFinder.blobs[i].centroid.y,0,camHeight,0,.75*ofGetWidth());
-            
-            ofBeginShape();
-            ofVertex(mapCentX, mapCentY);
-            ofVertex(mapPrevCentX, mapPrevCentY);
-            ofSetLineWidth(6);
-            ofEndShape(); 
-            ofFill();
-            ofCircle(mapCentX, mapCentY, 10);
+            for (int j=0; j<(int)contourFinder.blobs.size(); j++ ){
+                //Only draw lines if the distance between points is within that range
+                if (connectDist [i][j] < 200) {
+                    ofLine(ofMap(contourFinder.blobs[i].centroid.x,0, camWidth,0,ofGetWidth()),
+                           ofMap(contourFinder.blobs[i].centroid.y,0,camHeight,0,.75*ofGetWidth()),
+                           ofMap(contourFinder.blobs[j].centroid.x,0, camWidth,0,ofGetWidth()),
+                           ofMap(contourFinder.blobs[j].centroid.y,0,camHeight,0,.75*ofGetWidth()));
+                    //ofDrawBitmapString(ofToString(connectDist [i][j]), mapCentX,mapCentY);
+            }
+        }
              
-        }
-        mapPrevCentX = ofMap(contourFinder.blobs[i].centroid.x,0,camWidth,0,ofGetWidth());
-        mapPrevCentY = ofMap(contourFinder.blobs[i].centroid.y,0,camHeight,0,.75*ofGetWidth());
+        ofFill();
+        ofCircle(mapCentX, mapCentY, 10);
+        ofNoFill;
+               
     }  
+    /*
+    ofMesh mesh;
+    ofVec3f meshpoint = (0, 0, 0);
+    ofVec3f prevMeshpoint = (4, 4, 0);
     
-    ofDisableSmoothing();
-    for( int i=0; i<(int)contourFinder.blobs.size(); i++ ) {
-        ofNoFill();
-        ofBeginShape();
-        for( int j=0; j<contourFinder.blobs[i].nPts; j++ ) {
-            mappedX=ofMap(contourFinder.blobs[i].pts[j].x,0,camWidth,0,ofGetWidth());
-            mappedY=ofMap(contourFinder.blobs[i].pts[j].y,0,camHeight,0,.75*ofGetWidth());
-            ofSetLineWidth(1);
-            ofVertex(mappedX, mappedY);
-            
-        }
-        ofEndShape();
-        
-    }  
+    mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+    for(int j = 0; j < (int)contourFinder.blobs.size(); j++) {
+        ofColor cur(255, 255, 0, 255);
+        mesh.addColor(cur);
+        mesh.addVertex(meshpoint );
+        mesh.addVertex(meshpoint + ofVec3f(3, 0, 0));
+        mesh.addColor(cur);
+        mesh.addVertex(prevMeshpoint);
+        mesh.addVertex(prevMeshpoint - ofVec3f(3, 0, 0));
+    }
+    
+    mesh.draw();
+     */
+    
     ofDrawBitmapString("Threshold: " + ofToString(threshold), 20,20);
-     
+   
+	// Syphon Stuff
     
-    mainOutputSyphonServer.publishScreen();
-    
-    
-    
+    ofSetColor(255, 255, 255);
+
+	//mainOutputSyphonServer.publishScreen();
     
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
 
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){
 	switch (key){
 		case ' ':
 			bLearnBakground = true;
@@ -143,40 +177,41 @@ void testApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
+
--- src/testApp.h
+++ src/testApp.h
@@ -1,65 +1,74 @@
-#pragma once
+#ifndef _TEST_APP
+#define _TEST_APP
+
 
 #include "ofMain.h"
 #include "ofxSyphon.h"
 #include "ofxOpenCv.h"
 
-#define _USE_LIVE_VIDEO		// uncomment this to use a live camera
-								// otherwise, we'll use a movie file
-
 class testApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-		
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);		
-
-        #ifdef _USE_LIVE_VIDEO
-		  ofVideoGrabber 		vidGrabber;
-		#else
-		  ofVideoPlayer 		vidPlayer;
-		#endif
-
-        ofxCvColorImage			colorImg;
-
-        ofxCvGrayscaleImage 	grayImg;
-		ofxCvGrayscaleImage 	grayBg;
-		ofxCvGrayscaleImage 	grayDiff;
-        ofxCvContourFinder      contourFinder;
+	
+public:
+	
+	void setup();
+	void update();
+	void draw();
+	
+	void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);	
+	
+	float 	counter;
+	bool	bSmooth;
+	
+    ofTexture tex;
     
-        ofxSyphonServer mainOutputSyphonServer;
+	ofxSyphonServer mainOutputSyphonServer;
+	ofxSyphonServer individualTextureSyphonServer;
+	
+	ofxSyphonClient mClient;
+    
+    ofVideoGrabber 		vidGrabber;
 
-		int 				threshold;
-		bool				bLearnBakground;
     
-        int                 keyPress;
+    ofxCvColorImage			colorImg;
     
-        int                 camWidth;
-        int                 camHeight;
-        float               mappedX;
-        float               mappedY;
-        int                 ptAvg;
-        int                 ptSum;
+    ofxCvGrayscaleImage 	grayImg;
+    ofxCvGrayscaleImage 	grayBg;
+    ofxCvGrayscaleImage 	grayDiff;
+    ofxCvContourFinder      contourFinder;
     
-        int                 mapCentX;
-        int                 mapCentY;
-        int                 mapPrevCentX;
-        int                 mapPrevCentY;
+    int 				threshold;
+    bool				bLearnBakground;
     
-        float               linethick;
+    int                 keyPress;
     
-        int                 tog;
-
+    int                 camWidth;
+    int                 camHeight;
+    float               mappedX;
+    float               mappedY;
+    int                 ptAvg;
+    int                 ptSum;
+    
+    int                 mapCentX;
+    int                 mapCentY;
+    int                 mapPrevCentX;
+    int                 mapPrevCentY;
+    
+    int                 connectDist[20][20];
+    
+    float               linethick;
+    
+    int                 tog;
+    
 
 };
+
+#endif
 
